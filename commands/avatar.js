cd919b86ea688d65fbfd3550b8b67f5e5ecff2f3
Gianni <gianni.henriques@gmail.com>
2022-06-30 01:48:50 +0200

    banner command upgrade

--- commands/avatar.js
+++ commands/avatar.js
@@ -6,8 +6,7 @@ module.exports = {
 		.setDescription('Affiche votre photo de profil ou celle d\'un membre du serveur')
 		.addUserOption(option => option.setName('membre').setDescription('La photo de profil du membre voulu')),
 	async execute(_client, interaction) {
-		const user = interaction.options.getUser('membre');
-		if (user) return interaction.reply(`Photo de profil de ${user.username} :\n ${user.displayAvatarURL({ dynamic: true, size: 4096 })}`);
-		return interaction.reply(`Votre [photo de profil :](${interaction.user.displayAvatarURL({ dynamic: true, size: 4096 })})`);
+		const user = interaction.options.getUser('membre') ?? interaction.user;
+		if (user) return interaction.reply({content: `Photo de profil de ${user}[ : ](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`, allowedMentions: {parse: []}});
 	},
 };
--- commands/banner.js
+++ commands/banner.js
@@ -4,10 +4,16 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('banner')
 		.setDescription('Affiche votre bannière ou celle d\'un membre du serveur')
-		.addUserOption(option => option.setName('membre').setDescription('La bannière du membre voulu')),
+		.addUserOption(option => option.setName('membre').setDescription('La bannière du membre voulu'))
+		.addBooleanOption(option => option.setName('couleur').setDescription('Affiche la couleur de la bannière')), // Boolean: true & false
 	async execute(_client, interaction) {
 		let user = interaction.options.getUser('membre') ?? interaction.user;
 		user = await user.fetch();
-		if (user) return interaction.reply({content: `Bannière de ${user}[ : ](${user.bannerURL({ dynamic: true, size: 4096 })})`, allowedMentions: {parse: []}});
+		if (interaction.options.getBoolean('couleur')) {
+			const couleur = `La couleur de la bannière est : ${user.displayHexColor}.` ?? "L'utilisateur n'a pas de couleur personnalisée.";
+			return interaction.reply(couleur)
+		}
+		const message = (user.bannerURL()) ? `Bannière de ${user}[ : ](${user.bannerURL({dynamic: true, size: 4096})})` : `${user} n'a pas de bannière`;
+		if (user) return interaction.reply({content: message, allowedMentions: {parse: []}});
 	},
 };
