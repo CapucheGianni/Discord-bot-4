476af918f477808c241b074cfcc7f8c87e1c9901
Gianni Henriques <gianni.henriques@gmail.com>
2022-12-16 20:46:58 +0100

    + fix misplaced spaces

--- src/interactions/avatar.js
+++ src/interactions/avatar.js
@@ -8,6 +8,6 @@ module.exports = {
 	async execute(client, interaction) {
 		const user = interaction.options.getUser('membre') ?? interaction.user;
 
-		if (user) return interaction.reply({content: `Photo de profil de ${user} [:] (${user.displayAvatarURL({ dynamic: true, size: 4096 })})`, allowedMentions: {parse: []}});
+		if (user) return interaction.reply({content: `Photo de profil de ${user}[ : ](${user.displayAvatarURL({ dynamic: true, size: 4096 })})`, allowedMentions: {parse: []}});
 	},
 };
