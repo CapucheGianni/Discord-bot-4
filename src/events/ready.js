175549f6365e3d55c880c31bd9a2a65ca29af93b
Gianni Henriques <gianni.henriques@gmail.com>
2023-06-22 12:18:30 +0100

    feat: send another message in case of error when starting the bot

--- src/events/ready.js
+++ src/events/ready.js
@@ -16,8 +16,16 @@ module.exports = {
             });
             console.log(`${client.user.tag} is online !\nThe bot is available on ${client.guilds.cache.size} servers.`);
         } catch (err) {
+            const embed = new EmbedBuilder()
+                .setTitle('An error occured while sending the ready message.')
+                .setDescription(`\`\`\`js\n${err}\n\`\`\``)
+                .setColor(`#ff0000`)
+                .setTimestamp();
+
+            client.channels.cache.get('1121226924082077747').send({
+                embeds: [embed]
+            });
             console.error("An error occured while sending the ready message.");
-            console.error(err);
         }
     }
 };
