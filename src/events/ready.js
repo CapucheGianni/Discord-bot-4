fe83236e261643c481cf60246b55730565ce3914
Gianni Henriques <gianni.henriques@gmail.com>
2023-06-22 12:04:05 +0100

    feat: add a ready log when the bot is starting

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,23 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`${client.user.tag} is online !\nThe bot is available on ${client.guilds.cache.size} servers.`);
+        try {
+            const embed = new EmbedBuilder()
+                .setTitle('Bot is online !')
+                .setDescription(`The bot is available on ${client.guilds.cache.size} servers.`)
+                .setColor(`#00ff00`)
+                .setTimestamp();
+
+            client.channels.cache.get('1121226924082077747').send({
+                embeds: [embed]
+            });
+            console.log(`${client.user.tag} is online !\nThe bot is available on ${client.guilds.cache.size} servers.`);
+        } catch (err) {
+            console.error("An error occured while sending the ready message.");
+            console.error(err);
+        }
     }
 };
