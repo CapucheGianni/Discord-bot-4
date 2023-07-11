9078b5112298897037487904feb702d73f91105d
Gianni Henriques <gianni.henriques@gmail.com>
2023-07-11 18:09:23 +0200

    refactor(deployInteractions): use async/await instead of then/catch

--- src/deployInteractions.js
+++ src/deployInteractions.js
@@ -4,7 +4,7 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 require('dotenv').config();
 
-const deployInteractions = () => {
+const deployInteractions = async () => {
     const interactions = [];
     const interactionsPath = path.join(__dirname, 'interactions');
     const interactionFiles = fs.readdirSync(interactionsPath).filter(file => file.endsWith('.js'));
@@ -15,11 +15,16 @@ const deployInteractions = () => {
         interactions.push(interaction.data.toJSON());
     }
 
-    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+    const rest = new REST().setToken(process.env.TOKEN);
 
-    rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: interactions })
-        .then(() => console.log('Successfully registered application commands.'))
-        .catch(console.error);
+    try {
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] });
+        await rest.put(Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: interactions })
+            console.log('Successfully registered application commands.');
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 module.exports = deployInteractions;
