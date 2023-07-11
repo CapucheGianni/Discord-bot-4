d1b4af37b9cffa3ef6077807a997d3720b941995
Gianni Henriques <gianni.henriques@gmail.com>
2023-07-11 18:43:10 +0200

    feat(help cmd): use fields instead of desc and hide owner-only cmds

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,6 +5,7 @@ const { getPrefix } = require("../utils/setPrefix.js");
 module.exports = {
     name: "help",
     description: "Affiche la liste des commandes disponibles",
+    permissions: [],
     stats: {
         category: 'Utilitaire',
         usage: 'help [command]',
@@ -12,16 +13,39 @@ module.exports = {
     async run(client, command, args) {
         const commandName = args[0];
         const embed = new EmbedBuilder();
+        const cmdList = client.commands.filter(command => command.permissions[0] !== "OWNER");
 
         if (args[1])
             return command.reply("Merci de n'indiquer qu'une seule commande");
         if (commandName) {
             const cmd = client.commands.find(cmd => cmd.name === commandName);
 
-            if (!cmd)
+            if (!cmd || cmd.permissions[0] === "OWNER")
                 return command.reply(`La command \`${commandName}\` n'existe pas !`);
             embed.setTitle(`Commande \`${commandName}\` 📚`)
-            .setDescription(`**Catégorie:** ${cmd.stats.category}\n\n**Description:** ${cmd.description}\n\n**Usage:** \`${getPrefix()}${cmd.stats.usage}\``)
+            .setDescription("Voici les informations sur la commande demandée :")
+            .addFields(
+                {
+                    name: "Catégorie",
+                    value: cmd.stats.category,
+                    inline: true
+                },
+                {
+                    name: "Description",
+                    value: cmd.description,
+                    inline: true
+                },
+                {
+                    name: "Usage",
+                    value: `\`${getPrefix()}${cmd.stats.usage}\``,
+                    inline: true
+                },
+                {
+                    name: "Permissions",
+                    value: cmd.permissions.length ? cmd.permissions.map(perm => `\`${perm}\``).join(', ') : "Aucune permission requise",
+                    inline: true
+                }
+            )
             .setFooter({
                 text: `Commande effectuée par ${command.author.username} | ${client.user.username} V${version}`,
                 iconURL: command.author.displayAvatarURL({ dynamic: true })
@@ -31,8 +55,10 @@ module.exports = {
         } else {
             embed.setTitle("Liste des commandes 📚")
             .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-            .setDescription(`voici la liste des commandes disponibles :\n\n${client.commands.map(command => {
-                return `\`${getPrefix()}${command.name}\` - ${command.description}`
+            .setDescription(`voici la liste des commandes disponibles :\n\n${cmdList.map(command => {
+                if (command.permissions[0] === "OWNER")
+                    return;
+                return `\`${getPrefix()}${command.name}\` - ${command.description}`;
             }).join('\n')}`)
             .setFooter({
                 text: `Commande effectuée par ${command.author.username} | ${client.user.username} V${version}`,
