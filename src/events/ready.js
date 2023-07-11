fe0d0aac2aa1a946556a8a37cbef49a915cc45b5
Gianni Henriques <gianni.henriques@gmail.com>
2023-07-11 18:45:50 +0200

    feat(ready event): display approximate number of members available at startup

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,16 +5,21 @@ module.exports = {
     once: true,
     execute(client) {
         try {
+            let totalUsers = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
             const embed = new EmbedBuilder()
-                .setTitle('Bot is online !')
-                .setDescription(`The bot is available on ${client.guilds.cache.size} servers.`)
-                .setColor(`#00ff00`)
-                .setTimestamp();
+            .setTitle('Bot is online !')
+            .setDescription(`The bot is available on ${client.guilds.cache.size} servers.`)
+            .setColor(`#00ff00`)
+            .setTimestamp();
 
             client.channels.cache.get('1121226924082077747').send({
                 embeds: [embed]
             });
-            console.log(`${client.user.tag} is online !\nThe bot is available on ${client.guilds.cache.size} servers.`);
+            if (totalUsers >= 10) {
+                totalUsers /= 100;
+                totalUsers = Math.round(totalUsers) * 100;
+            }
+            console.log(`${client.user.tag} is online !\nThe bot is available on ${client.guilds.cache.size} servers with approximatively ${totalUsers} members.`);
         } catch (err) {
             const embed = new EmbedBuilder()
                 .setTitle('An error occured while sending the ready message.')
