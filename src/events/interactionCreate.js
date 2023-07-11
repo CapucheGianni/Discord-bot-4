375cb9b536fc78a615189518a0b7078adfe20b0e
Gianni Henriques <gianni.henriques@gmail.com>
2023-07-11 18:48:24 +0200

    refactor(interactionCreate): separate interactions cooldown init in an another function

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,25 +1,52 @@
 const { Events, EmbedBuilder, Collection } = require('discord.js');
 
-const interactionSuccess = async (client, interaction) => {
+const interactionLog = async (client, interaction) => {
     const embed = new EmbedBuilder()
     .setTitle("Interaction exécutée ✅")
     .setDescription(`**Auteur:** ${interaction.user} (${interaction.user.id})\n**Salon:** ${interaction.channel} (${interaction.channel.id})\n**Serveur:** ${interaction.guild} (${interaction.guild.id})\n**Interaction:** ${interaction.commandName}`)
     .setFooter({
         text: `Interaction exécutée par ${interaction.user.username} | ${client.user.username}`,
         iconURL: interaction.user.displayAvatarURL({ dynamic: true })
-        })
-        .setTimestamp()
-        .setColor(`#00ff00`);
-        await client.channels.cache.get("1121226924082077747").send({
-            embeds: [embed]
-        });
-    };
+    })
+    .setTimestamp()
+    .setColor(`#00ff00`);
 
-    module.exports = {
-        name: Events.InteractionCreate,
-        async execute(client, interaction) {
-        const { cooldowns } = client;
+    await client.channels.cache.get("1121226924082077747").send({
+        embeds: [embed]
+    });
+};
 
+const initInteractionsCooldowns = (client, interaction, getInteraction) => {
+    const { cooldowns } = client;
+
+    if (!cooldowns.has(getInteraction.data.name)) {
+        cooldowns.set(getInteraction.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(getInteraction.data.name);
+    const defaultCooldownDuration = 3;
+    const cooldownAmount = (getInteraction.cooldown ?? defaultCooldownDuration) * 1000;
+
+    if (timestamps.has(interaction.user.id)) {
+        const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const expiredTimestamp = Math.round(expirationTime / 1000);
+            interaction.reply({
+                content: `Please wait, you are on a cooldown for \`${getInteraction.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+                ephemeral: true
+            });
+            return 1;
+        }
+    }
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+};
+
+module.exports = {
+    name: Events.InteractionCreate,
+    async execute(client, interaction) {
         if (!interaction.isCommand())
             return;
 
@@ -29,35 +56,18 @@ const interactionSuccess = async (client, interaction) => {
             console.error(`No interaction matching ${interaction.commandName} was found.`);
             return;
         }
-        if (!cooldowns.has(getInteraction.data.name)) {
-            cooldowns.set(getInteraction.data.name, new Collection());
-        }
 
-        const now = Date.now();
-        const timestamps = cooldowns.get(getInteraction.data.name);
-        const defaultCooldownDuration = 3;
-        const cooldownAmount = (getInteraction.cooldown ?? defaultCooldownDuration) * 1000;
+        const isCd = initInteractionsCooldowns(client, interaction, getInteraction);
 
-        if (timestamps.has(interaction.user.id)) {
-            const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
-
-            if (now < expirationTime) {
-                const expiredTimestamp = Math.round(expirationTime / 1000);
-                return interaction.reply({
-                    content: `Please wait, you are on a cooldown for \`${getInteraction.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
-                    ephemeral: true
-                });
-            }
-        }
-        timestamps.set(interaction.user.id, now);
-        setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+        if (isCd)
+            return;
         try {
             const currentDate = new Date();
             const time = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
             const date = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
 
             await getInteraction.execute(client, interaction);
-            await interactionSuccess(client, interaction);
+            await interactionLog(client, interaction);
             console.log(`${interaction.commandName} interaction executed by ${interaction.user.username} (${interaction.user.id}) in ${interaction.guild.name} (${interaction.guild.id}) at ${date} ${time}`);
         } catch (error) {
             console.error(`Error executing ${interaction.commandName}`);
