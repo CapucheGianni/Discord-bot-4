9434538382439cb3396b358315b3f67d16f42eb5
Gianni Henriques <gianni.henriques@gmail.com>
2023-07-12 22:21:25 +0200

    refactor: remove the line which was force deleting every guild interaction

--- src/deployInteractions.js
+++ src/deployInteractions.js
@@ -18,7 +18,6 @@ const deployInteractions = async () => {
     const rest = new REST().setToken(process.env.TOKEN);
 
     try {
-        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] });
         await rest.put(Routes.applicationCommands(process.env.CLIENT_ID),
             { body: interactions })
             console.log('Successfully registered application commands.');
