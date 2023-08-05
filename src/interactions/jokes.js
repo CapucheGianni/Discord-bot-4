333fd02aae3ce447d32791271042fed59535e17b
Gianni Henriques <gianni.henriques@gmail.com>
2023-08-05 20:49:23 +0200

    feat: you can chose whether you want to get jokes or not

--- src/interactions/jokes.js
+++ src/interactions/jokes.js
@@ -9,39 +9,43 @@ module.exports = {
 		.setDescription("Choisissez si vous voulez que le bot réponde à certains de vos messages ou non.")
 		.addBooleanOption(option => option.setName("activer").setDescription("Active ou désactive les blagues.")),
 	stats: {
-		category: "Image",
-		usage: "/avatar [membre]",
+		category: "Utilitaire",
+		usage: "/jokes [true/false]",
 		permissions: []
 	},
 	async execute(client, interaction) {
-		const choice = interaction.options.getBoolean("activer");
+		try {
+			const choice = interaction.options.getBoolean("activer");
 
-		if (choice) {
-			await prisma.user.update({
-				where: {
-					userid: interaction.user.id
-				},
-				data: {
-					userjokes: true
-				}
-			});
-			interaction.reply({
-				content: "Les blagues ont été activées avec succès !",
-				ephemeral: true
-			});
-		} else {
-			await prisma.user.update({
-				where: {
-					userid: interaction.user.id
-				},
-				data: {
-					userjokes: false
-				}
-			});
-			interaction.reply({
-				content: "Les blagues ont été désactivées avec succès !",
-				ephemeral: true
-			});
-		}
+			if (choice) {
+				await prisma.user.update({
+					where: {
+						id: interaction.user.id
+					},
+					data: {
+						jokes: true
+					}
+				});
+				interaction.reply({
+					content: "Les blagues ont été activées avec succès !",
+					ephemeral: true
+				});
+			} else {
+				await prisma.user.update({
+					where: {
+						id: interaction.user.id
+					},
+					data: {
+						jokes: false
+					}
+				});
+				interaction.reply({
+					content: "Les blagues ont été désactivées avec succès !",
+					ephemeral: true
+				});
+			}
+		} catch (e) {
+			console.error(e);
+		};
 	}
 };
