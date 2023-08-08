9ec6356dc315a3e4a07f9c6493356b02f5e442ee
Gianni Henriques <gianni.henriques@gmail.com>
2023-08-09 00:34:03 +0200

    feat: jokes can now be enabled/disabled on a whole server

--- src/interactions/jokes.js
+++ src/interactions/jokes.js
@@ -4,94 +4,94 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const serverUpdate = async (guildId, choice, interaction) => {
-	if (choice) {
-		await prisma.server.update({
-			where: {
-				id: guildId
-			},
-			data: {
-				jokes: true
-			}
-		});
-		interaction.reply({
-			content: "Les blagues ont été activées sur le serveur avec succès !",
-			ephemeral: true
-		});
-	} else {
-		await prisma.server.update({
-			where: {
-				id: guildId
-			},
-			data: {
-				jokes: false
-			}
-		});
-		interaction.reply({
-			content: "Les blagues ont été désactivées sur le serveur avec succès !",
-			ephemeral: true
-		});
-	}
+    if (choice) {
+        await prisma.server.update({
+            where: {
+                id: guildId
+            },
+            data: {
+                jokes: true
+            }
+        });
+        interaction.reply({
+            content: "Les blagues ont été activées sur le serveur avec succès !",
+            ephemeral: true
+        });
+    } else {
+        await prisma.server.update({
+            where: {
+                id: guildId
+            },
+            data: {
+                jokes: false
+            }
+        });
+        interaction.reply({
+            content: "Les blagues ont été désactivées sur le serveur avec succès !",
+            ephemeral: true
+        });
+    }
 };
 
 const userUpdate = async (userId, choice, interaction) => {
-	if (choice) {
-		await prisma.user.update({
-			where: {
-				id: userId
-			},
-			data: {
-				jokes: true
-			}
-		});
-		interaction.reply({
-			content: "Les blagues ont été activées avec succès !",
-			ephemeral: true
-		});
-	} else {
-		await prisma.user.update({
-			where: {
-				id: userId
-			},
-			data: {
-				jokes: false
-			}
-		});
-		interaction.reply({
-			content: "Les blagues ont été désactivées avec succès !",
-			ephemeral: true
-		});
-	}
+    if (choice) {
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                jokes: true
+            }
+        });
+        interaction.reply({
+            content: "Les blagues ont été activées avec succès !",
+            ephemeral: true
+        });
+    } else {
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                jokes: false
+            }
+        });
+        interaction.reply({
+            content: "Les blagues ont été désactivées avec succès !",
+            ephemeral: true
+        });
+    }
 };
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName("jokes")
-		.setDescription("Choisissez si vous voulez que le bot réponde à certains de vos messages ou non.")
-		.addBooleanOption(option => option.setName("serveur").setDescription("Active ou désactive les blagues sur le serveur."))
-		.addBooleanOption(option => option.setName("activer").setDescription("Active ou désactive les blagues.")),
-	stats: {
-		category: "Utilitaire",
-		usage: "/jokes [true/false]",
-		permissions: []
-	},
-	async execute(client, interaction) {
-		try {
-			const choice = interaction.options.getBoolean("activer");
-			const server = interaction.options.getBoolean("serveur");
+    data: new SlashCommandBuilder()
+        .setName("jokes")
+        .setDescription("Choisissez si vous voulez que le bot réponde à certains de vos messages ou non.")
+        .addBooleanOption((option) => option.setName("serveur").setDescription("Active ou désactive les blagues sur le serveur."))
+        .addBooleanOption((option) => option.setName("activer").setDescription("Active ou désactive les blagues.")),
+    stats: {
+        category: "Utilitaire",
+        usage: "/jokes [true/false]",
+        permissions: []
+    },
+    execute(client, interaction) {
+        try {
+            const choice = interaction.options.getBoolean("activer");
+            const server = interaction.options.getBoolean("serveur");
 
-			if (server) {
-				if (!interaction.member.permissions.has("ManageGuild")) {
-					return interaction.reply({
-						content: "Vous n'avez pas les permissions nécessaires pour modifier le préfixe !",
-						ephemeral: true
-					});
-				}
-				serverUpdate(interaction.guild.id, choice, interaction);
-			} else {
-				userUpdate(interaction.user.id, choice, interaction);
-			}
-		} catch (e) {
-			console.error(e);
-		};
-	}
+            if (server) {
+                if (!interaction.member.permissions.has("ManageGuild")) {
+                    return interaction.reply({
+                        content: "Vous n'avez pas les permissions nécessaires pour modifier le préfixe !",
+                        ephemeral: true
+                    });
+                }
+                serverUpdate(interaction.guild.id, choice, interaction);
+            } else {
+                userUpdate(interaction.user.id, choice, interaction);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
 };
