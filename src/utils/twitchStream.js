19981504d4c6012a9ec8e369bd86de6262a57ee9
Gianni Henriques <gianni.henriques@gmail.com>
2023-08-10 22:45:46 +0200

    feat: twitch notification is able to detect when stream game is changed

--- src/utils/twitchStream.js
+++ src/utils/twitchStream.js
@@ -1,6 +1,9 @@
 const { EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 let check = 0;
+const infos = {
+    oldGame: ""
+};
 const { twitch } = require("../../settings.json");
 
 const getTwitchAccessToken = async () => {
@@ -21,12 +24,49 @@ const getTwitchAccessToken = async () => {
     }
 }
 
+const setEmbed = (client, title, viewer_count, game_name) => {
+    const embed = new EmbedBuilder()
+        .setTitle(`${title}`)
+        .setImage(`https://static-cdn.jtvnw.net/previews-ttv/live_user_${twitch.TWITCH_USER_LOGIN}-1920x1080.jpg?cacheBypass=${Date.now()}`)
+        .addFields(
+            {
+                name: 'Viewers',
+                value: `${viewer_count}`,
+                inline: true
+            },
+            {
+                name: 'Lien',
+                value: `https://twitch.tv/${twitch.TWITCH_USER_LOGIN}`,
+                inline: true
+            },
+            {
+                name: 'Jeu',
+                value: `${game_name}`,
+                inline: true
+            }
+        )
+        .setFooter({
+            text: `Stream de ${twitch.TWITCH_USER_LOGIN} | ${client.user.username}`,
+            iconURL: client.user.displayAvatarURL({ dynamic: true })
+        })
+        .setTimestamp()
+        .setColor(`#6441a5`);
+
+    return embed;
+};
+
+const sendMessage = async (client, content, embed) => {
+    await client.channels.cache.get("1121226924082077747").send({
+        content: content,
+        embeds: [ embed ],
+        allowedMentions: { parse: [ 'everyone' ] }
+    });
+};
+
 const getTwitchStream = async (client) => {
     const twitchAccessToken = await getTwitchAccessToken();
     const twitchAPIURL = 'https://api.twitch.tv/helix/streams';
-    const params = {
-        user_login: twitch.TWITCH_USER_LOGIN
-    };
+    const params = { user_login: twitch.TWITCH_USER_LOGIN };
     const headers = {
         'Client-ID': twitch.TWITCH_CLIENT_ID,
         'Authorization': `Bearer ${twitchAccessToken}`
@@ -34,55 +74,34 @@ const getTwitchStream = async (client) => {
 
     setInterval(async () => {
         try {
-            const response = await axios.get(twitchAPIURL, { params, headers });
+            const response = await axios.get(twitchAPIURL, {
+                params,
+                headers
+            });
             const { data } = response;
 
             if (data.data.length) {
-                const { title, viewer_count, game_name } = data.data[0];
-                const embed = new EmbedBuilder()
-                    .setTitle(`${title}`)
-                    .setImage(`https://static-cdn.jtvnw.net/previews-ttv/live_user_${twitch.TWITCH_USER_LOGIN}-1920x1080.jpg?cacheBypass=${Date.now()}`)
-                    .addFields(
-                        {
-                            name: 'Viewers',
-                            value: `${viewer_count}`,
-                            inline: true
-                        },
-                        {
-                            name: 'Lien',
-                            value: `https://twitch.tv/${twitch.TWITCH_USER_LOGIN}`,
-                            inline: true
-                        },
-                        {
-                            name: 'Jeu',
-                            value: `${game_name}`,
-                            inline: true
-                        }
-                    )
-                    .setFooter({
-                        text: `Stream de ${twitch.TWITCH_USER_LOGIN} | ${client.user.username}`,
-                        iconURL: client.user.displayAvatarURL({ dynamic: true })
-                    })
-                    .setTimestamp()
-                    .setColor(`#6441a5`)
+                const { title, viewer_count, game_name } = data.data[ 0 ];
+                const embed = setEmbed(client, title, viewer_count, game_name);
 
-                if (check === 1)
-                    return;
-                check = 1;
-                await client.channels.cache.get("1137739326822826065").send({
-                    content: `Coucou @everyone! **${twitch.TWITCH_USER_LOGIN}** est en live sur Twitch !`,
-                    embeds: [embed],
-                    allowedMentions: {
-                        parse: ['everyone']
+                if (check === 1) {
+                    if (game_name !== infos.oldGame && check === 1) {
+                        await sendMessage(client, `Changement de plan, **${twitch.TWITCH_USER_LOGIN}** joue maintenant à __${game_name}__!`, embed);
+                        infos.oldGame = game_name;
                     }
-                });
+                    return;
+                }
+                check = 1;
+                await sendMessage(client, `Coucou @everyone! **${twitch.TWITCH_USER_LOGIN}** est en live sur Twitch!`, embed);
+                infos.oldGame = game_name;
             } else {
+                infos.oldGame = "";
                 check = 0;
             }
         } catch (e) {
             console.log(e);
         }
-    }, 1000 * 60);
+    }, 1000 * 60 * 5);
 };
 
 module.exports = getTwitchStream;
