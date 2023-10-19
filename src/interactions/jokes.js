0f5a467a17df2a5eb082455b9768fe7b3ea9d4c1
Gianni Henriques <gianni.henriques@gmail.com>
2023-10-19 22:46:15 +0200

    feat(jokes): rework of the joke interaction & new channel table in the db

--- src/interactions/jokes.js
+++ src/interactions/jokes.js
@@ -2,72 +2,89 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { prisma } = require('../db/main.js');
 const { interactionsIds } = require('../../settings.json');
 
-const serverUpdate = async (guildId, choice, interaction) => {
-    if (choice) {
-        await prisma.server.update({
-            where: {
-                id: guildId
-            },
-            data: {
-                jokes: true
-            }
-        });
-        interaction.reply({
-            content: "Les blagues ont été activées sur le serveur avec succès !",
-            ephemeral: true
-        });
-    } else {
-        await prisma.server.update({
-            where: {
-                id: guildId
-            },
-            data: {
-                jokes: false
-            }
-        });
-        interaction.reply({
-            content: "Les blagues ont été désactivées sur le serveur avec succès !",
-            ephemeral: true
-        });
-    }
-};
+const serverUpdate = async (interaction) => {
+    const choice = interaction.options.getBoolean("enable") ?? false;
+    let res = "";
 
-const userUpdate = async (userId, choice, interaction) => {
-    if (choice) {
-        await prisma.user.update({
-            where: {
-                id: userId
-            },
-            data: {
-                jokes: true
-            }
-        });
-        interaction.reply({
-            content: "Les blagues ont été activées avec succès !",
-            ephemeral: true
-        });
-    } else {
-        await prisma.user.update({
-            where: {
-                id: userId
-            },
-            data: {
-                jokes: false
-            }
-        });
-        interaction.reply({
-            content: "Les blagues ont été désactivées avec succès !",
-            ephemeral: true
-        });
-    }
-};
+    await prisma.server.update({
+        where: {
+            id: interaction.guildId
+        },
+        data: {
+            jokes: choice
+        }
+    });
+    choice ? res = "Les blagues ont été activées avec succès" : res = "Les blagues ont été désactivées avec succès";
+    return interaction.reply(res);
+}
+
+const channelUpdate = async (interaction) => {
+    const choice = interaction.options.getBoolean("enable") ?? false;
+    const channel = interaction.options.getChannel("picker");
+    let res = "";
+
+    await prisma.channel.upsert({
+        where: {
+            id: channel.id
+        },
+        update: {
+            jokes: choice
+        },
+        create: {
+            id: channel.id,
+            name: channel.name,
+            jokes: choice,
+            serverId: interaction.guildId
+        }
+    });
+    choice ? res = `Les blagues ont été activées sur <#${channel.id}> avec succès` : res = `Les blagues ont été désactivées sur <#${channel.id}> avec succès`;
+    return interaction.reply(res);
+}
+
+const userUpdate = async (interaction) => {
+    const choice = interaction.options.getBoolean("enable") ?? false;
+    let res = "";
+
+    await prisma.user.update({
+        where: {
+            id: interaction.user.id
+        },
+        data: {
+            jokes: choice
+        }
+    });
+    choice ? res = "Kaide pourra maintenant répondre à vos messages" : res = "Kaide ne pourra plus répondre à vos messages";
+    return interaction.reply(res);
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("jokes")
         .setDescription("Choisissez si vous voulez que le bot réponde à certains de vos messages.")
-        .addBooleanOption((option) => option.setName("serveur").setDescription("Choisissez cette option si vous voulez activer/désactiver les blagues à l'échelle du serveur."))
-        .addBooleanOption((option) => option.setName("activer").setDescription("Active ou désactive les blagues.")),
+
+        .addSubcommand((command) => command
+            .setName("server")
+            .setDescription("Active ou désactive les blagues à l'échelle du serveur")
+            .addBooleanOption((option) => option
+                .setName("enable")
+                .setDescription("true/false")))
+
+        .addSubcommand((command) => command
+            .setName("channel")
+            .setDescription("Active ou désactive les blagues sur un salon spécifique")
+            .addChannelOption((option) => option
+                .setName("picker")
+                .setDescription("Nom du salon"))
+            .addBooleanOption((option) => option
+                .setName("enable")
+                .setDescription("true/false")))
+
+        .addSubcommand((command) => command
+            .setName("user")
+            .setDescription("Active ou désactive les blagues à l'échelle de l'utilisateur")
+            .addBooleanOption((option) => option
+                .setName("enable")
+                .setDescription("true/false"))),
     stats: {
         category: "Utilitaire",
         permissions: [],
@@ -75,19 +92,15 @@ module.exports = {
     },
     execute(client, interaction) {
         try {
-            const choice = interaction.options.getBoolean("activer");
-            const server = interaction.options.getBoolean("serveur");
+            const subcommands = interaction.options.getSubcommand();
 
-            if (server) {
-                if (!interaction.member.permissions.has("ManageGuild")) {
-                    return interaction.reply({
-                        content: "Vous n'avez pas les permissions nécessaires pour modifier le préfixe !",
-                        ephemeral: true
-                    });
-                }
-                serverUpdate(interaction.guild.id, choice, interaction);
-            } else {
-                userUpdate(interaction.user.id, choice, interaction);
+            switch (subcommands) {
+                case 'server':
+                    return serverUpdate(interaction);
+                case 'channel':
+                    return channelUpdate(interaction);
+                case 'user':
+                    return userUpdate(interaction);
             }
         } catch (e) {
             throw new Error(e);
