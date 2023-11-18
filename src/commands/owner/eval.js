bba04241f21cfb19c81f6539fccbed54f3cbe2c4
Gianni Henriques <gianni.henriques@gmail.com>
2023-11-18 22:31:11 +0100

    fix(eval-cmd): error was threw away when the output was an object

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -12,15 +12,13 @@ module.exports = {
         if (!args[0]) return command.react('❌');
         if (args.join(' ').includes('token')) return command.channel.send('Nope');
 
-        const result = new Promise((resolve) => resolve(eval(args.join(' '))));
-
-        return result.then((output) => {
-            if (output.includes(process.env.TOKEN)) output = output.replace(process.env.TOKEN, 'Comment ça mon reuf ?');
-            command.channel.send(output, { code: 'js' });
+        return new Promise((resolve) => resolve(eval(args.join(' ')))).then((output) => {
+            if (typeof output === 'string' && output.includes(process.env.TOKEN)) output = output.replace(process.env.TOKEN, 'Comment ça mon reuf ?');
+            command.channel.send(`\`\`\`js\n${output}\`\`\``);
         }).catch((e) => {
             e = e.toString();
             if (e.includes(process.env.TOKEN)) e = e.replace(process.env.TOKEN, 'Comment ça mon reuf ?');
-            command.channel.send(e, { code: 'js' });
-        })
+            command.channel.send(`\`\`\`js\n${e}\`\`\``);
+        });
     }
 };
