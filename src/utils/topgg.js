bd32b3445ed3fff91a22174fb0a3f2cc9a3e83a4
Gianni Henriques <gianni.henriques@gmail.com>
2024-01-16 14:48:13 +0100

    refactor(tophgg): use the right npm package provided by the topgg doc

--- src/utils/topgg.js
+++ src/utils/topgg.js
@@ -1,14 +1,16 @@
-const DBL = require('dblapi.js');
+const { AutoPoster } = require('topgg-autoposter')
 const topGg = require('../../settings.json');
 
 const setTopGg = (client) => {
     if (client.user.username !== 'Kaide') return;
 
-    const dbl = new DBL(topGg.TOKEN, client);
+    const ap = AutoPoster(topGg.topgg.TOKEN, client)
 
-    dbl.on('posted', () => {});
-    dbl.on('error', e => {
-      console.error(`Oops! ${e}`);
+    ap.on('posted', () => {
+        console.log("All useful informations were pushed to the top.gg API.");
+    });
+    ap.on('error', e => {
+        console.error(`Oops! ${e}`);
     });
 };
 
