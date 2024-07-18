42db05f035445cc1fa16cfef3f68d809d60ec455
Gianni Henriques <gianni.henriques@gmail.com>
2024-07-18 19:31:10 +0200

    feat(modules): use ES modules instead of commonJS modules & improve commands/events/interactions declarations using classes and decorators

---
+++ src/classes/ModuleImports.ts
@@ -0,0 +1,149 @@
+import 'reflect-metadata'
+import { glob } from 'glob'
+import { Collection, PermissionsString, SlashCommandBuilder, AutocompleteInteraction } from 'discord.js'
+import path from 'path'
+import { fileURLToPath, pathToFileURL } from 'url'
+import { Bot } from './Bot.js'
+import { TDiscordEvents, TEventType } from '../types/DiscordEvents.js'
+import { getMetadata } from '../utils/Decorators.js'
+import { TCategory } from '../types/Command.js'
+import { Logger } from './Logger.js'
+import Database from './Database.js'
+
+const logger = Logger.getInstance('')
+
+export abstract class Module {
+    public abstract name: string
+    public abstract execute(client: Bot, ...args: any[]): Promise<void>
+}
+
+export abstract class CommandModule extends Module {
+    public name: string = ''
+    public description!: string
+    public cooldown!: number
+    public permissions!: PermissionsString[]
+    public category!: TCategory
+    public usage!: string
+    public aliases!: string[]
+
+    constructor() {
+        super()
+        Object.keys(this).forEach(key => {
+            const optionKey = key as keyof typeof this
+            this[optionKey] = getMetadata(optionKey, this.constructor)
+        })
+    }
+}
+
+export abstract class EventModule extends Module {
+    public name: TDiscordEvents = 'raw'
+    public eventType!: TEventType
+
+    constructor() {
+        super()
+        Object.keys(this).forEach(key => {
+            const optionKey = key as keyof typeof this
+            this[optionKey] = getMetadata(optionKey, this.constructor)
+        })
+    }
+}
+
+export abstract class InteractionModule extends Module {
+    public name: string = ''
+    public description!: string
+    public permissions!: PermissionsString[]
+    public category!: TCategory
+    public usage!: string
+    public data!: SlashCommandBuilder
+
+    constructor() {
+        super()
+        Object.keys(this).forEach(key => {
+            const optionKey = key as keyof typeof this
+            this[optionKey] = getMetadata(optionKey, this.constructor)
+        })
+    }
+
+    public abstract autoComplete(client: Bot, interaction: AutocompleteInteraction): Promise<void>
+}
+
+export class ModuleImports {
+    private _commands: Collection<string, CommandModule> = new Collection()
+    private _interactions: Collection<string, InteractionModule> = new Collection()
+    private _events: Collection<string, EventModule> = new Collection()
+
+    public async loadModules() {
+        const filePath = fileURLToPath(import.meta.url)
+        const srcDirPath = path.join(filePath, '..', '..', '..')
+        const modulePaths = await glob(`${srcDirPath}/build/{events,commands,interactions}/**/*.{ts,js}`)
+
+        for (const path of modulePaths) {
+            const module = await import(pathToFileURL(path).href)
+            if (module.default) {
+                const instance: Module = new module.default()
+
+                if (instance instanceof CommandModule)
+                    this._commands.set(instance.name, instance)
+                if (instance instanceof EventModule)
+                    this._events.set(instance.name, instance)
+                if (instance instanceof InteractionModule)
+                    this._interactions.set(instance.name, instance)
+            }
+        }
+    }
+
+    public get commands(): Collection<string, CommandModule> {
+        return this._commands
+    }
+
+    public get interactions(): Collection<string, InteractionModule> {
+        return this._interactions
+    }
+
+    public get events(): Collection<string, EventModule> {
+        return this._events
+    }
+
+    public eventListener(client: Bot) {
+        for (const [eventName, eventModule] of this._events) {
+            if (eventModule.eventType === 'once')
+                client.once(eventName, (...args) => eventModule.execute(client, ...args))
+            else
+                client.on(eventName, (...args) => eventModule.execute(client, ...args))
+        }
+    }
+
+    public async upsertInteractionsIntoDb(db: Database): Promise<void> {
+        const t = await db.database.transaction()
+
+        try {
+            for (const [interactionName, interactionModule] of this._interactions) {
+                await db.Interaction.upsert(
+                    { name: interactionName },
+                    { transaction: t }
+                )
+            }
+            await t.commit()
+        } catch (error: any) {
+            await t.rollback()
+            logger.simpleError(error)
+        }
+    }
+
+    public async upsertCommandsIntoDb(db: Database): Promise<void> {
+        const t = await db.database.transaction()
+
+        try {
+            for (const [commandName, commandModule] of this._commands) {
+                await db.Command.upsert(
+                    { name: commandName },
+                    { transaction: t }
+                )
+            }
+            await t.commit()
+        } catch (error: any) {
+            await t.rollback()
+            logger.simpleError(error)
+        }
+    }
+}
---
+++ src/utils/Decorators.ts
@@ -0,0 +1,68 @@
+import { TDiscordEvents, TEventType } from '../types/DiscordEvents.js'
+import { PermissionsString, SlashCommandBuilder, SlashCommandOptionsOnlyBuilder } from 'discord.js'
+import { CommandModule } from '../classes/ModuleImports.js'
+import { TCategory } from '../types/Command.js'
+
+interface event {
+    name: TDiscordEvents,
+    eventType: TEventType
+}
+
+interface command {
+    name: string,
+    description: string,
+    cooldown: number,
+    permissions: PermissionsString[],
+    category: TCategory,
+    usage: string,
+    aliases: string[]
+}
+
+interface interaction {
+    name: string,
+    description: string,
+    permissions: PermissionsString[],
+    category: TCategory,
+    usage: string,
+    data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder
+}
+
+const addMetadata = (metadataKey: string, metadataValue: any) => {
+    return (constructor: Function) => {
+        Reflect.defineMetadata(metadataKey, metadataValue, constructor)
+    }
+}
+
+export const getMetadata = (key: any, constructor: Function) => {
+    return Reflect.getMetadata(key, constructor)
+}
+
+export const EventDecorator = (options: event) => {
+    return (constructor: Function) => {
+        addMetadata('type', 'event')(constructor)
+        Object.keys(options).forEach(key => {
+            const optionKey = key as keyof typeof options
+            addMetadata(key, options[optionKey])(constructor)
+        })
+    }
+}
+
+export const CommandDecorator = (options: command) => {
+    return (constructor: typeof CommandModule) => {
+        addMetadata('type', 'command')(constructor)
+        Object.keys(options).forEach(key => {
+            const optionKey = key as keyof typeof options
+            addMetadata(key, options[optionKey])(constructor)
+        })
+    }
+}
+
+export const InteractionDecorator = (options: interaction) => {
+    return (constructor: Function) => {
+        addMetadata('type', 'interaction')(constructor)
+        Object.keys(options).forEach(key => {
+            const optionKey = key as keyof typeof options
+            addMetadata(key, options[optionKey])(constructor)
+        })
+    }
+}
