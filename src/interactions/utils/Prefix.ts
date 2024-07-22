e5ff6bcac7b0aa9cce31f9d1c6455410022391b8
Gianni Henriques <gianni.henriques@gmail.com>
2024-07-22 15:20:33 +0200

    feat(interactions): implemented urlshorten

--- src/interactions/utils/Prefix.ts
+++ src/interactions/utils/Prefix.ts
@@ -33,29 +33,23 @@ export default class PrefixInteraction extends InteractionModule {
     public async autoComplete(client: Bot, interaction: AutocompleteInteraction): Promise<void> { }
 
     public async execute(client: Bot, interaction: CommandInteraction): Promise<any> {
-        try {
+        const options = interaction.options as CommandInteractionOptionResolver
+        const newPrefix = options.getString('prefix')
+        const prefix = (await client.database.getGuild(interaction.guildId!)).prefix
 
-            const options = interaction.options as CommandInteractionOptionResolver
-            const newPrefix = options.getString('prefix')
-            const prefix = (await client.database.getGuild(interaction.guildId!)).prefix
-
-            if (newPrefix) {
-                if (!(interaction.member as GuildMember).permissions.has('ManageGuild')) {
-                    return interaction.reply({
-                        content: 'Vous n\'avez pas les permissions nécessaires pour modifier le préfixe du serveur',
-                        ephemeral: true
-                    })
-                }
-                client.database.Server.update(
-                    { prefix: newPrefix },
-                    { where: { id: interaction.guildId! } }
-                )
-                return interaction.reply(`Le prefix de ${client.user!.username} est désormais \`${newPrefix}\`.`)
+        if (newPrefix) {
+            if (!(interaction.member as GuildMember).permissions.has('ManageGuild')) {
+                return interaction.reply({
+                    content: 'Vous n\'avez pas les permissions nécessaires pour modifier le préfixe du serveur',
+                    ephemeral: true
+                })
             }
-            interaction.reply(`Le préfixe de ${client.user!.username} est : \`${prefix}\``)
-        } catch (error) {
-            interaction.reply(`Une erreur est survenue lors de l'éxécution de l'intéraction.`)
-            logger.log(client, `An error occured while executing the prefix interaction: ${error}`, 'error')
+            client.database.Server.update(
+                { prefix: newPrefix },
+                { where: { id: interaction.guildId! } }
+            )
+            return interaction.reply(`Le prefix de ${client.user!.username} est désormais \`${newPrefix}\`.`)
         }
+        interaction.reply(`Le préfixe de ${client.user!.username} est : \`${prefix}\``)
     }
 }
---
+++ src/interactions/utils/UrlShorten.ts
@@ -0,0 +1,57 @@
+import { Bot } from '../../classes/Bot.js'
+import {
+    AutocompleteInteraction,
+    SlashCommandBuilder,
+    CommandInteraction,
+    PermissionsBitField,
+    CommandInteractionOptionResolver
+} from 'discord.js'
+import { InteractionDecorator } from '../../utils/Decorators.js'
+import { InteractionModule } from '../../classes/ModuleImports.js'
+import { Logger } from '../../classes/Logger.js'
+
+import settings from '../../../settings.json' assert { type: 'json' }
+
+const logger = Logger.getInstance('')
+
+@InteractionDecorator({
+    name: 'urlshorten',
+    description: 'Renvoie un nouveau lien pointant sur le lien choisi.',
+    cooldown: 3,
+    category: 'utils',
+    usage: 'urlshorten <url>',
+    data: new SlashCommandBuilder()
+        .setName('urlshorten')
+        .setDescription('Renvoie un nouveau lien pointant sur le lien choisi.')
+        .addStringOption(option => option
+            .setName('url')
+            .setDescription('Le lien à modifier (http://, https://)')
+            .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.SendMessages)
+})
+export default class UrlShortenInteraction extends InteractionModule {
+    public async autoComplete(client: Bot, interaction: AutocompleteInteraction): Promise<void> { }
+
+    public async execute(client: Bot, interaction: CommandInteraction): Promise<any> {
+        const options = interaction.options as CommandInteractionOptionResolver
+        const urlToShorten = options.getString('url')
+        const rebrandlyUrl = 'https://api.rebrandly.com/v1/links'
+        const data = JSON.stringify({ destination: urlToShorten })
+        const response = await fetch(rebrandlyUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': settings.rebrandly.API_KEY
+            },
+            body: data
+        })
+
+        if (!response.ok)
+            return interaction.reply(`Merci de fournir un url valide`)
+
+        const jsonResponse = await response.json()
+
+        return interaction.reply(`https://${jsonResponse.shortUrl}`)
+    }
+}
