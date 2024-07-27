bfebdd3bf6ea0a8249a396dce22364a2ddc3fe56
Gianni Henriques <gianni.henriques@gmail.com>
2024-07-27 15:07:52 +0200

    feat(events): puns can configurable for each servers with different types of detection

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -154,59 +154,90 @@ export default class MessageCreate extends EventModule {
     }
 
     private async _getPun(client: Bot, message: Message): Promise<void> {
-        const puns = [
-            [['quoi'], ['feur', 'coubeh']],
-            [['oui'], ['stiti']],
-            [['non'], ['bril']],
-            [['a', 'ah', 'aah', 'aaa'], ['b']],
-            [['c'], ['d', 'des pantalons']],
-            [['e'], ['z ratio', 'f']],
-            [['z'], ['Mdrrr imagine t\'es allé jusqu\'au bout']],
-            [['si', 'six'], ['rène comme toi ma belle']],
-            [['crois'], ['sant']],
-            [['tg'], ['v']],
-            [['ouais'], ['stern']],
-            [['mais'], ['juin']],
-            [['juillet'], ['août (mois goatesque souhaitez moi mon anniversaire le 20 svp)']],
-            [['hein', 'un'], ['deux']],
-            [['trois'], ['soleil t\'as bougé au cachot', 'quatre']],
-            [['toi'], ['lettes', 'ture']],
-            [['you'], ['π (pi pour les deux trois débiles du fond genre Mathis au hasard)']],
-            [['fuck'], ['let\'s do it baby 😉😏']],
-            [['merde'], ['<@492561801913827349> on t\'appelle']],
-            [['mince'], ['c\'est gianni ça']],
-            [['ok', 'okay', 'oke'], ['sur glace', 'sur gazon']],
-            [['euh'], ['f']],
-            [['g'], ['ianni hihihihi']],
-            [['trop'], ['pico']],
-            [['oh'], ['tarie', 'tiste']],
-            [['gg'], ['bg']],
-            [['chaud'], ['cacao', 'chocolat']],
-            [['fdp', 'ftg', 'connard', 'salope', 'pute', 'con'], ['parle mieux ou jte baise', 'ratio', 'jte ez', 'la violence c\'est mal les enfants']],
-            [['comment'], ['dant', 'tateur']],
-            [['moi'], ['sonneuse']],
-            [['bonsoir'], ['Pariiis']],
-            [['ntm'], ['on parle pas des mamans']],
-            [['ratio'], ['Flop likez tous svp']],
-            [['ca'], ['ca 💩']]
-        ]
-        const user = await client.database.User.findByPk(message.author.id)
-        const channel = await client.database.Channel.findByPk(message.channelId)
-        const server = await client.database.Server.findByPk(message.guildId!)
+        try {
+            const user = (await client.database.User.findByPk(message.author.id))?.get()
+            const channel = (await client.database.Channel.findByPk(message.channelId))?.get()
+            const server = (await client.database.Server.findByPk(message.guildId!))?.get()
 
-        if (!isUser(user?.get()) || !isChannel(channel?.get()) || !isServer(server?.get()))
-            return
-
-        const messageAsArray = message.content.replace(/[.,\/#!?$%\^&\*;:{}=_`~()]/g, ' ').split(' ')
-        const lastWord = messageAsArray.reverse().find((word) => /[a-zA-Z0-9]/.test(word))
-
-        if (!lastWord)
-            return
-        for (const pun of puns) {
-            if (pun[0].includes(lastWord)) {
-                message.reply(pun[1][Math.floor(Math.random() * pun[1].length)])
+            if (!isUser(user) || !isChannel(channel) || !isServer(server))
                 return
-            }
+            if (!user.jokes || !channel.jokes || !server.jokes)
+                return
+
+            const messageAsArray = message.content.replace(/[,.[\]\/#!?$%\^&\*;:{}=_`~()\\]/g, '').trim().split(' ')
+            const firstWord = messageAsArray[0]
+            const lastWord = messageAsArray.reverse()[0]
+
+            if (await this._endsWith(client, message, lastWord))
+                return
+            if (await this._includes(client, message, messageAsArray))
+                return
+            await this._startsWith(client, message, firstWord)
+        } catch (error: any) {
+            logger.log(client, error, 'error')
         }
+    }
+
+    private async _endsWith(client: Bot, message: Message, lastWord: string): Promise<boolean> {
+        const punsEndsWith = (await client.database.Pun.findAll({
+            where: {
+                serverId: message.guildId!,
+                type: 'endsWith'
+            }
+        })).map(pun => pun.get())
+        if (!punsEndsWith || !punsEndsWith.length)
+            return false
+
+        const matchingPuns = punsEndsWith.filter(pun => pun.toFind === lastWord)
+
+        if (matchingPuns.length > 0) {
+            const randomPun = matchingPuns[Math.floor(Math.random() * matchingPuns.length)]
+
+            await message.reply(randomPun.toAnswer)
+            return true
+        }
+        return false
+    }
+
+    private async _includes(client: Bot, message: Message, messageAsArray: string[]): Promise<boolean> {
+        const punsIncludes = (await client.database.Pun.findAll({
+            where: {
+                serverId: message.guildId!,
+                type: 'includes'
+            }
+        })).map(pun => pun.get())
+        if (!punsIncludes || !punsIncludes.length)
+            return false
+
+        const matchingPuns = punsIncludes.filter(pun => messageAsArray.includes(pun.toFind))
+
+        if (matchingPuns.length > 0) {
+            const randomPun = matchingPuns[Math.floor(Math.random() * matchingPuns.length)]
+
+            await message.reply(randomPun.toAnswer)
+            return true
+        }
+        return false
+    }
+
+    private async _startsWith(client: Bot, message: Message, firstWord: string): Promise<boolean> {
+        const punsStartsWith = (await client.database.Pun.findAll({
+            where: {
+                serverId: message.guildId!,
+                type: 'startsWith'
+            }
+        })).map(pun => pun.get())
+        if (!punsStartsWith || !punsStartsWith.length)
+            return false
+
+        const matchingPuns = punsStartsWith.filter(pun => pun.toFind === firstWord)
+
+        if (matchingPuns.length > 0) {
+            const randomPun = matchingPuns[Math.floor(Math.random() * matchingPuns.length)]
+
+            await message.reply(randomPun.toAnswer)
+            return true
+        }
+        return false
     }
 }
